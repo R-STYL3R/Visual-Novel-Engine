bd7345cd3a064545c33389110217187386ceac45
R_STYLER <Tornike01>
2022-05-17 17:37:31 +0300

    Frontend version 0.2

--- UI/src/main/java/ru/rstyler/ui/MainMenu.java
+++ UI/src/main/java/ru/rstyler/ui/MainMenu.java
@@ -3,16 +3,20 @@ package ru.rstyler.ui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class MainMenu extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        Font.loadFont(getClass().getResourceAsStream("/fonts/Montserrat-Light.ttf"), 16);
         FXMLLoader fxmlLoader = new FXMLLoader(MainMenu.class.getResource("MainMenu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setTitle("Void Entry");
         stage.setScene(scene);
-        scene.getStylesheets (). add (getClass (). getResource ("MainMenu.css").toExternalForm ());
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("MainMenu.css")).toExternalForm());
         stage.show();
     }
 }
